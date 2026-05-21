63d1b6c6428b14630d91d43d7f30a456e86645bd
Yuliabaiguskarova <_email__>
2026-05-22 00:30:39 +0500

    add solution

---
+++ blogicum/blog/views.py
@@ -0,0 +1,173 @@
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.db.models import Count
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils import timezone
+
+from .forms import CommentForm, PostForm, UserForm
+from .models import Category, Comment, Post, User
+
+POSTS_PER_PAGE = 10
+
+
+def get_published_posts(annotate_comments=True):
+    queryset = Post.objects.select_related(
+        'author', 'location', 'category',
+    ).filter(
+        pub_date__lte=timezone.now(),
+        is_published=True,
+        category__is_published=True,
+    )
+    if annotate_comments:
+        queryset = queryset.annotate(
+            comment_count=Count('comments')
+        ).order_by('-pub_date')
+    return queryset
+
+
+def index(request):
+    post_list = get_published_posts()
+    paginator = Paginator(post_list, POSTS_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/index.html', {'page_obj': page_obj})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author != request.user:
+        post = get_object_or_404(
+            get_published_posts(annotate_comments=False),
+            pk=post_id,
+        )
+    comments = post.comments.select_related('author')
+    form = CommentForm()
+    return render(request, 'blog/detail.html', {
+        'post': post,
+        'form': form,
+        'comments': comments,
+    })
+
+
+def category_posts(request, category_slug):
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True,
+    )
+    post_list = get_published_posts().filter(category=category)
+    paginator = Paginator(post_list, POSTS_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/category.html', {
+        'category': category,
+        'page_obj': page_obj,
+    })
+
+
+def profile(request, username):
+    profile_user = get_object_or_404(User, username=username)
+    if request.user == profile_user:
+        posts = profile_user.post_set.select_related(
+            'author', 'location', 'category',
+        ).annotate(
+            comment_count=Count('comments')
+        ).order_by('-pub_date')
+    else:
+        posts = get_published_posts().filter(author=profile_user)
+    paginator = Paginator(posts, POSTS_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/profile.html', {
+        'profile': profile_user,
+        'page_obj': page_obj,
+    })
+
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:profile', username=request.user.username)
+    else:
+        form = UserForm(instance=request.user)
+    return render(request, 'blog/user.html', {'form': form})
+
+
+@login_required
+def create_post(request):
+    form = PostForm(request.POST or None, files=request.FILES or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('blog:profile', username=request.user.username)
+    return render(request, 'blog/create.html', {'form': form})
+
+
+@login_required
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author != request.user:
+        return redirect('blog:post_detail', post_id=post_id)
+    form = PostForm(request.POST or None, files=request.FILES or None,
+                    instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('blog:post_detail', post_id=post_id)
+    return render(request, 'blog/create.html', {'form': form})
+
+
+@login_required
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author != request.user:
+        return redirect('blog:post_detail', post_id=post_id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('blog:index')
+    form = PostForm(instance=post)
+    return render(request, 'blog/create.html', {'form': form})
+
+
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+    return redirect('blog:post_detail', post_id=post_id)
+
+
+@login_required
+def edit_comment(request, post_id, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if comment.author != request.user:
+        return redirect('blog:post_detail', post_id=post_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:post_detail', post_id=post_id)
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'blog/comment.html', {
+        'form': form,
+        'comment': comment,
+    })
+
+
+@login_required
+def delete_comment(request, post_id, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if comment.author != request.user:
+        return redirect('blog:post_detail', post_id=post_id)
+    if request.method == 'POST':
+        comment.delete()
+        return redirect('blog:post_detail', post_id=post_id)
+    return render(request, 'blog/comment.html', {'comment': comment})
